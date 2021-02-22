a4c28ccf77c5c282384c9cd9467f5a0b6fac7e3e
Vladymyr Grebenko <vhrebenko@gmail.com>
2021-02-22 23:44:29 +0200

    add js events filters and tabs card product

--- scripts/product-card.js
+++ scripts/product-card.js
@@ -8,7 +8,13 @@
   };
   const controllsColor = document.querySelectorAll(".filter-color__link");
   const controllsSize = document.querySelectorAll(".filter-size__link");
+  const tabsControlls = document.querySelectorAll(".description-tab__link");
+  const tabs = document.querySelectorAll(".description-list__item");
+
   const productImg = document.querySelector(".product__img");
+
+  const activeClassLinkTab = "description-tab__item--active";
+  const activeClassTab = "description-list__item-type--active";
   const activeClassColor = "filter-color__item--type-select";  
   const activeClassSize = "filter-size__item--type-select";
 
@@ -31,6 +37,25 @@
           link.closest(".filter-size__item").classList.remove(activeClassSize);
         })
         control.closest(".filter-size__item").classList.add(activeClassSize);           
+      })
+    })
+
+    tabsControlls.forEach(function(control) {      
+      control.addEventListener("click", function(e) {
+        e.preventDefault();   
+        const tabId = control.getAttribute("data-tab");    
+        const activeTab = document.querySelector(`.${tabId}`); 
+        
+        tabsControlls.forEach(function(link) {          
+          link.closest(".description-tab__item").classList.remove(activeClassLinkTab);
+        })
+        control.closest(".description-tab__item").classList.add(activeClassLinkTab);    
+        
+        tabs.forEach(function(tab) {          
+          tab.classList.remove(activeClassTab);
+        })
+        
+        activeTab.classList.add(activeClassTab)
       })
     })
 
